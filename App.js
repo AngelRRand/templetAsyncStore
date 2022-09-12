4569c0aaf14a6e55b8b5afdbdb37e2e8596351a3
AngelRRand <horacio27rodriguez_cba@hotmail.com>
2022-09-11 22:18:37 -0300

    styles

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, Button, TextInput, TouchableHighlight } from 'react-native';
 
 export default function App() {
   return (
@@ -6,7 +6,16 @@ export default function App() {
 
       <StatusBar style="auto" />
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <TextInput
+          style={styles.input}
+        />
+        <Button
+          title='guardar'
+          color='#333'
+        />
+        <TouchableHighlight style={styles.btnEliminar}>
+          <Text style={styles.textoEliminar}>Eliminar Nombre</Text>
+        </TouchableHighlight>
       </View>
 
     </>
@@ -21,4 +30,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderColor:'#666',
+    borderBottomWidth:1,
+    width:300,
+    height:40,
+  },
+  btnEliminar:{
+    backgroundColor:'red',
+    marginTop:20,
+    padding:10,
+  },
+  textoEliminar:{
+    color: 'white',
+    fontWeight:'bold',
+    textAlign:'center',
+    textTransform:'uppercase',
+    width: 300,
+  }
 });
